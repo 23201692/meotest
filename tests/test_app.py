42f7f543859a146c31921a4ea83992a9d8ecab19
23201692 <23201692@live.preco.ru>
2026-05-07 04:22:41 +0000

    Fix tests import

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,8 @@
-import pytest
-import json
+import sys
 import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pytest
 from models import Book, Library
 
 
